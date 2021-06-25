e0afe0b49e8379a244c93a3094d2b485b4533ef5
miyoshi <lepus3443@gmail.com>
2021-06-26 08:38:38 +0900

    message

---
+++ database/migrations/2021_06_24_113559_create_commits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commits', function (Blueprint $table) {
+            $table->id();
+            $table->string('share_id');
+            $table->string('user_id');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commits');
+    }
+}
